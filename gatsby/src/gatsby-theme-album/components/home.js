e1ae330de852e439a286d4065e7a83ce631f1f3f
esgomezm <egomez@igc.gulbenkian.pt>
2024-03-25 22:56:01 +0200

    gatsby style

--- gatsby/src/gatsby-theme-album/components/home.js
+++ gatsby/src/gatsby-theme-album/components/home.js
@@ -1,9 +1,14 @@
 import React from "react"
+import { StaticImage } from "gatsby-plugin-image"
 
 const Home = () => {
   return (
-    <p>Welcome!</p>
+    <div>
+        <StaticImage className="logo" src="https://raw.githubusercontent.com/HenriquesLab/DL4MicEverywhere/main/docs/images/policy.png" alt="DL4MicEverywhere offers an easy-to-use gateway to deep learning techniques for bioimage analysis." placeholder="blurred" fit="contain"/>
+    </div>
   )
 }
 
 export default Home
+
+
--- gatsby/src/gatsby-theme-album/components/logo.js
+++ gatsby/src/gatsby-theme-album/components/logo.js
@@ -3,6 +3,6 @@ import { StaticImage } from "gatsby-plugin-image"
 
 export default function Logo() {
   return (
-  <StaticImage className="logo" src="../images/logo.png" alt="catalog logo" placeholder="blurred" fit="contain"/>
+  <StaticImage className="logo" src="https://raw.githubusercontent.com/HenriquesLab/DL4MicEverywhere/main/docs/logo/dl4miceverywhere-logo-small.png" alt="catalog logo" placeholder="blurred" fit="contain"/>
   );
 }
