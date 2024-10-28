6478400a215d9044da338c55d08d242011649c13
Iván Hidalgo <ivanhcenalmor@gmail.com>
2024-10-28 17:58:50 +0000

    Rename workflows and add debugging

--- .tools/python_tools/check_dl4mic_versions.py
+++ .tools/python_tools/check_dl4mic_versions.py
@@ -4,6 +4,9 @@ import os
 
 def main(dl4miceverywhere_path=None, dl4miceverywhere_album_path=None):
 
+    print('Calling main')
+    print(os.listidir('.'))
+
     # Check if paths to the repositories have been provided and if not, it will be assumed that they are located in the same folder.
     if dl4miceverywhere_path is None:
         dl4miceverywhere_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', '..', '..', 'DL4MicEverywhere'))
