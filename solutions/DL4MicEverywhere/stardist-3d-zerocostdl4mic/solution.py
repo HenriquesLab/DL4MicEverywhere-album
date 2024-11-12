40d1f52af24677398396f0e3c4775ac0215cb6e5
Iván <ivanhcenalmor@gmail.com>
2024-11-12 16:55:04 +0000

    Adding new/updated DL4MicEverywhere_stardist-3d-zerocostdl4mic_1.15.3

--- solutions/DL4MicEverywhere/stardist-3d-zerocostdl4mic/solution.py
+++ solutions/DL4MicEverywhere/stardist-3d-zerocostdl4mic/solution.py
@@ -22,9 +22,9 @@ def install():
 
     # Clone the DL4MicEverywhere repository
     clone_url = "https://github.com/HenriquesLab/DL4MicEverywhere"
-    repo_path = get_app_path().joinpath("DL4MicEverywhere")
-    Repo.clone_from(clone_url, repo_path)
-    assert (repo_path.exists())
+    repository_path = get_app_path().joinpath("DL4MicEverywhere")
+    Repo.clone_from(clone_url, repository_path)
+    assert (repository_path.exists())
 
     # URL of the notebook you want to download
     notebook_url = "https://raw.githubusercontent.com/HenriquesLab/ZeroCostDL4Mic/master/Colab_notebooks/StarDist_3D_ZeroCostDL4Mic.ipynb"
@@ -47,7 +47,7 @@ def install():
     python_command = ["python", ".tools/notebook_autoconversion/transform.py", "-p", f"{get_app_path()}", "-n", "StarDist_3D_ZeroCostDL4Mic.ipynb", "-s"]
     python_command += section_to_remove
 
-    subprocess.run(python_command, cwd=to)
+    subprocess.run(python_command, cwd=repository_path)
     subprocess.run(["mv", get_app_path().joinpath("colabless_StarDist_3D_ZeroCostDL4Mic.ipynb"), get_app_path().joinpath("StarDist_3D_ZeroCostDL4Mic.ipynb")])
 
     # Remove the cloned DL4MicEverywhere repository
@@ -55,7 +55,7 @@ def install():
         os.system(f'rmdir /s /q "{to}"')
     else:
         # rmtree has no permission to do this on Windows
-        shutil.rmtree(to) 
+        shutil.rmtree(repository_path) 
 
 def run():
     from album.runner.api import get_args, get_app_path
@@ -95,7 +95,7 @@ if gpu_access:
     dependencies = """
 - python=3.10
 - cudatoolkit=11.8.0
-- cudnn=8.6.0
+- cudnn=8.9.2
 - pip
 - pkg-config
 """
@@ -140,13 +140,15 @@ dependencies:
     - tifffile==2023.7.18
     - tqdm==4.65.0
     - wget==3.2
+    - nbformat==5.9.2
+    - jupyterlab==3.4.0
 """
 
 setup(
     group="DL4MicEverywhere",
     name="stardist-3d-zerocostdl4mic",
     version="1.15.3",
-    solution_creators=["DL4Mic team", "album team"],
+    solution_creators=["DL4MicEverywhere team", "album team"],
     title="stardist-3d-zerocostdl4mic implementation.",
     description="3D instance segmentation of oval objects (ie nuclei). StarDist is a deep-learning method that can be used to segment cell nuclei in 3D (xyz) images. Note - visit the ZeroCostDL4Mic wiki to check the original publications this network is based on and make sure you cite these.",
     documentation="https://raw.githubusercontent.com/HenriquesLab/ZeroCostDL4Mic/master/BioimageModelZoo/README.md",
