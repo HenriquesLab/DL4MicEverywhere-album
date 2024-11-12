568d304deed15e182e8fa7f398160bebf1e89f1c
Iván <ivanhcenalmor@gmail.com>
2024-11-12 16:07:31 +0000

    Adding new/updated DL4MicEverywhere_detectron2-2d-zerocostdl4mic_1.15.1

--- solutions/DL4MicEverywhere/detectron2-2d-zerocostdl4mic/solution.py
+++ solutions/DL4MicEverywhere/detectron2-2d-zerocostdl4mic/solution.py
@@ -22,9 +22,9 @@ def install():
 
     # Clone the DL4MicEverywhere repository
     clone_url = "https://github.com/HenriquesLab/DL4MicEverywhere"
-    repo_path = get_app_path().joinpath("DL4MicEverywhere")
-    Repo.clone_from(clone_url, repo_path)
-    assert (repo_path.exists())
+    repository_path = get_app_path().joinpath("DL4MicEverywhere")
+    Repo.clone_from(clone_url, repository_path)
+    assert (repository_path.exists())
 
     # URL of the notebook you want to download
     notebook_url = "https://raw.githubusercontent.com/HenriquesLab/ZeroCostDL4Mic/master/Colab_notebooks/Detectron2_2D_ZeroCostDL4Mic.ipynb"
@@ -47,7 +47,7 @@ def install():
     python_command = ["python", ".tools/notebook_autoconversion/transform.py", "-p", f"{get_app_path()}", "-n", "Detectron2_2D_ZeroCostDL4Mic.ipynb", "-s"]
     python_command += section_to_remove
 
-    subprocess.run(python_command, cwd=to)
+    subprocess.run(python_command, cwd=repository_path)
     subprocess.run(["mv", get_app_path().joinpath("colabless_Detectron2_2D_ZeroCostDL4Mic.ipynb"), get_app_path().joinpath("Detectron2_2D_ZeroCostDL4Mic.ipynb")])
 
     # Remove the cloned DL4MicEverywhere repository
@@ -55,7 +55,7 @@ def install():
         os.system(f'rmdir /s /q "{to}"')
     else:
         # rmtree has no permission to do this on Windows
-        shutil.rmtree(to) 
+        shutil.rmtree(repository_path) 
 
 def run():
     from album.runner.api import get_args, get_app_path
@@ -95,7 +95,7 @@ if gpu_access:
     dependencies = """
 - python=3.9
 - cudatoolkit=11.8.0
-- cudnn=8.6.0
+- cudnn=8.9.2
 - pip
 - pkg-config
 """
@@ -132,13 +132,16 @@ dependencies:
     - torchvision==0.15.1
     - wget==3.2
     - PyYAML==6.0
+    - nbformat==5.9.2
+    - ipywidgets==8.1.0
+    - jupyterlab==3.4.0
 """
 
 setup(
     group="DL4MicEverywhere",
     name="detectron2-2d-zerocostdl4mic",
     version="1.15.1",
-    solution_creators=["DL4Mic team", "album team"],
+    solution_creators=["DL4MicEverywhere team", "album team"],
     title="detectron2-2d-zerocostdl4mic implementation.",
     description="Object detection of 2D images. Detectron2 is an object detection network developed by Facebook AI Research, which identifies objects in images and draws bounding boxes around them.",
     documentation="https://raw.githubusercontent.com/HenriquesLab/ZeroCostDL4Mic/master/BioimageModelZoo/README.md",
