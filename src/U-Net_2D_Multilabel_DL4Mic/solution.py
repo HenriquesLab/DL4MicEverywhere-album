5a56d2df00d6b43b7b737bfa7b7d0e289eec4da8
IvanHCenalmor <IvanHCenalmor@users.noreply.github.com>
2024-11-15 22:40:41 +0000

    GitHub Action - Update the notebook version on the configuration

--- src/U-Net_2D_Multilabel_DL4Mic/solution.py
+++ src/U-Net_2D_Multilabel_DL4Mic/solution.py
@@ -152,7 +152,7 @@ setup(
     title="u-net-2d-multilabel-zerocostdl4mic implementation.",
     description="2D semantic segmentation. U-Net is an encoder-decoder architecture originally used for image segmentation. The first half of the U-Net architecture is a downsampling convolutional neural network which acts as a feature extractor from input images. The other half upsamples these results and restores an image by combining results from downsampling with the upsampled images. Note - visit the ZeroCostDL4Mic wiki to check the original publications this network is based on and make sure you cite these.",
     documentation="https://raw.githubusercontent.com/HenriquesLab/ZeroCostDL4Mic/master/BioimageModelZoo/README.md",
-    tags=['colab', 'notebook', 'u-net', 'segmentation', 'semantic-segmentation', 'multilabel', 'ZeroCostDL4Mic', '2D', 'dl4miceverywhere'],
+    tags=['ARM64', 'AMD64', 'colab', 'notebook', 'u-net', 'segmentation', 'semantic-segmentation', 'multilabel', 'ZeroCostDL4Mic', '2D', 'dl4miceverywhere'],
     args=[{
         "name": "path",
         "type": "string",
