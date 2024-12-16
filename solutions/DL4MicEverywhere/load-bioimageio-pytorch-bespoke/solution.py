db6f8c3948285d0a5f001139c7b77a0564d2dfce
Iván <ivanhcenalmor@gmail.com>
2024-12-16 21:15:33 +0000

    Adding new/updated DL4MicEverywhere_load-bioimageio-pytorch-bespoke_1.6.0

---
+++ solutions/DL4MicEverywhere/load-bioimageio-pytorch-bespoke/solution.py
@@ -0,0 +1,154 @@
+###album catalog: cellcanvas
+
+# Based on https://github.com/HenriquesLab/DL4MicEverywhere/blob/main/notebooks/ZeroCostDL4Mic_notebooks/bioimageio-pytorch/configuration.yaml
+# and https://github.com/betaseg/solutions/blob/main/solutions/io.github.betaseg/cellsketch-plot/solution.py
+
+from album.runner.api import setup
+import subprocess
+
+try:
+    subprocess.check_output('nvidia-smi')
+    gpu_access = True
+except Exception: 
+    gpu_access = False
+
+def install():
+    from album.runner.api import get_app_path
+    from git import Repo
+    import subprocess
+    import requests
+    import shutil
+    import os
+
+    # Clone the DL4MicEverywhere repository
+    clone_url = "https://github.com/HenriquesLab/DL4MicEverywhere"
+    repository_path = get_app_path().joinpath("DL4MicEverywhere")
+    Repo.clone_from(clone_url, repository_path)
+    assert (repository_path.exists())
+
+    # URL of the notebook you want to download
+    notebook_url = "https://raw.githubusercontent.com/HenriquesLab/DL4MicEverywhere/main/notebooks/Bespoke_notebooks/bioimageio-pytorch/load_bioimageio_pytorch.ipynb"
+    
+    notebook_path = get_app_path().joinpath("load_bioimageio_pytorch.ipynb")
+    notebook_path.parent.mkdir(parents=True, exist_ok=True)
+
+    response = requests.get(notebook_url)
+    response.raise_for_status()
+    
+    with open(notebook_path, 'wb') as notebook_file:
+        notebook_file.write(response.content)
+    
+    assert notebook_path.exists(), "Notebook download failed"
+
+    # Convert the notebook to its colabless form
+    section_to_remove = "1.1. 1.2."
+    section_to_remove = section_to_remove.split(' ')
+    
+    python_command = ["python", ".tools/notebook_autoconversion/transform.py", "-p", f"{get_app_path()}", "-n", "load_bioimageio_pytorch.ipynb", "-s"]
+    python_command += section_to_remove
+
+    subprocess.run(python_command, cwd=repository_path)
+    subprocess.run(["mv", get_app_path().joinpath("colabless_load_bioimageio_pytorch.ipynb"), get_app_path().joinpath("load_bioimageio_pytorch.ipynb")])
+
+    # Remove the cloned DL4MicEverywhere repository
+    if os.name == 'nt':
+        os.system(f'rmdir /s /q "{to}"')
+    else:
+        # rmtree has no permission to do this on Windows
+        shutil.rmtree(repository_path) 
+
+def run():
+    from album.runner.api import get_args, get_app_path
+    import subprocess
+    import os
+
+    # Fetch arguments and paths
+    args = get_args()
+    app_path = get_app_path()
+    
+    # Path to the downloaded notebook
+    notebook_path = app_path.joinpath("load_bioimageio_pytorch.ipynb")
+
+    # Ensure the notebook exists
+    assert notebook_path.exists(), "Notebook does not exist"
+
+    # Output path for running the notebook
+    output_path = args.path
+    os.makedirs(output_path, exist_ok=True)
+    print(f"Saving output to {output_path}")
+
+    # Set the LD_LIBRARY_PATH to allow TensorFlow to find the CUDA libraries
+    global gpu_access
+    if gpu_access:
+        os.environ["LD_LIBRARY_PATH"] = f"{os.environ['LD_LIBRARY_PATH']}:{os.environ['CONDA_PREFIX']}/lib"
+
+    # Optionally, launch the Jupyter notebook to show the results
+    subprocess.run(["jupyter", "lab", str(notebook_path)], cwd=str(output_path))
+
+if gpu_access:
+    channels = """
+- conda-forge
+- nvidia
+- anaconda
+- defaults
+"""
+    dependencies = """
+- python=3.9
+- cudatoolkit=11.2.2
+- cudnn=8.9.2
+- pip
+- pkg-config
+"""
+else:
+    channels = """
+- conda-forge
+- defaults
+"""
+    dependencies = f"""
+- python=3.9
+- pip
+- pkg-config
+"""
+
+env_file = f"""
+channels:
+{channels}
+dependencies:
+{dependencies}
+- pip:
+    - GitPython==3.1.43 
+    - bioimageio.core==0.6.9
+    - matplotlib==3.9.0
+    - imageio==2.31.2
+    - numpy==1.23.5
+    - onnxruntime==1.18.0
+    - torch==2.2.0
+    - pooch==1.8
+    - marshmallow==3.21.3
+    - nbformat==5.9.2
+    - ipywidgets==8.1.0
+    - jupyterlab==3.4.0
+"""
+
+setup(
+    group="DL4MicEverywhere",
+    name="load-bioimageio-pytorch-bespoke",
+    version="1.6.0",
+    solution_creators=["DL4MicEverywhere team", "album team"],
+    title="load-bioimageio-pytorch-bespoke implementation.",
+    description="Notebook to load models from the BioImage Model Zoo in PyTorch",
+    documentation="https://raw.githubusercontent.com/HenriquesLab/ZeroCostDL4Mic/master/BioimageModelZoo/README.md",
+    tags=['ARM64', 'AMD64', 'colab', 'notebook', 'general-notebook', 'pytorch', 'ZeroCostDL4Mic', 'dl4miceverywhere'],
+    args=[{
+        "name": "path",
+        "type": "string",
+        "default": ".",
+        "description": "What is your working path?"
+    }],
+    cite=[{'doi': 'https://doi.org/10.1101/2022.06.07.495102', 'text': 'Wei Ouyang, Fynn Beuttenmueller, Estibaliz Gómez-de-Mariscal, Constantin Pape, et al., BioImage Model Zoo: A Community-Driven Resource for Accessible Deep Learning in BioImage Analysis, bioRxiv 2022'}, {'doi': 'https://doi.org/10.1101/2023.11.19.567606', 'text': 'Iván Hidalgo-Cenalmor, Joanna W. Pylvänäinen, Mariana G Ferreira, et al., DL4MicEverywhere: Deep learning for microscopy made flexible, shareable, and reproducible, bioRxiv 2023'}],
+    album_api_version="0.5.1",
+    covers=[],
+    run=run,
+    install=install,
+    dependencies={"environment_file": env_file},
+)
