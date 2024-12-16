e464fbcda34d63292c82be6f73b9a442864821b1
IvanHCenalmor <IvanHCenalmor@users.noreply.github.com>
2024-12-16 18:57:58 +0000

    GitHub Action - Update the notebook version on the configuration

--- src/ZeroCostDL4Mic_notebooks/DecoNoising_2D_DL4Mic/solution.py
+++ src/ZeroCostDL4Mic_notebooks/DecoNoising_2D_DL4Mic/solution.py
@@ -147,7 +147,7 @@ setup(
     title="deconoising-2d-zerocostdl4mic implementation.",
     description="Self-supervised denoising of 2D images. DecoNoising 2D is deep-learning method that can be used to denoise 2D microscopy images. By running this notebook, you can train your own network and denoise your images. Note - visit the ZeroCostDL4Mic wiki to check the original publications this network is based on and make sure you cite these.",
     documentation="https://raw.githubusercontent.com/HenriquesLab/ZeroCostDL4Mic/master/BioimageModelZoo/README.md",
-    tags=['colab', 'notebook', 'DecoNoising', 'denoising', 'ZeroCostDL4Mic', '2D', 'dl4miceverywhere'],
+    tags=['AMD64', 'colab', 'notebook', 'DecoNoising', 'denoising', 'ZeroCostDL4Mic', '2D', 'dl4miceverywhere'],
     args=[{
         "name": "path",
         "type": "string",
