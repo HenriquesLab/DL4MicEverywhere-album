88d85fd05000c093d8d5e8d13502324320bd9091
Iván <ivanhcenalmor@gmail.com>
2024-12-16 21:25:32 +0000

    Adding new/updated DL4MicEverywhere_deep-storm-2d-zerocostdl4mic_1.13.3

---
+++ solutions/DL4MicEverywhere/deep-storm-2d-zerocostdl4mic/solution.py
@@ -0,0 +1,162 @@
+###album catalog: cellcanvas
+
+# Based on https://github.com/HenriquesLab/DL4MicEverywhere/blob/main/notebooks/ZeroCostDL4Mic_notebooks/Deep-STORM_2D_DL4Mic/configuration.yaml
+# and https://github.com/betaseg/solutions/blob/main/solutions/io.github.betaseg/cellsketch-plot/solution.py
+
+from album.runner.api import setup
+import subprocess
+
+try:
+    subprocess.check_output('nvidia-smi')
+    gpu_access = True
+except Exception: 
+    gpu_access = False
+
+def install():
+    from album.runner.api import get_app_path
+    from git import Repo
+    import subprocess
+    import requests
+    import shutil
+    import os
+
+    # Clone the DL4MicEverywhere repository
+    clone_url = "https://github.com/HenriquesLab/DL4MicEverywhere"
+    repository_path = get_app_path().joinpath("DL4MicEverywhere")
+    Repo.clone_from(clone_url, repository_path)
+    assert (repository_path.exists())
+
+    # URL of the notebook you want to download
+    notebook_url = "https://raw.githubusercontent.com/HenriquesLab/ZeroCostDL4Mic/master/Colab_notebooks/Deep-STORM_2D_ZeroCostDL4Mic.ipynb"
+    
+    notebook_path = get_app_path().joinpath("Deep-STORM_2D_ZeroCostDL4Mic.ipynb")
+    notebook_path.parent.mkdir(parents=True, exist_ok=True)
+
+    response = requests.get(notebook_url)
+    response.raise_for_status()
+    
+    with open(notebook_path, 'wb') as notebook_file:
+        notebook_file.write(response.content)
+    
+    assert notebook_path.exists(), "Notebook download failed"
+
+    # Convert the notebook to its colabless form
+    section_to_remove = "2. 6.4."
+    section_to_remove = section_to_remove.split(' ')
+    
+    python_command = ["python", ".tools/notebook_autoconversion/transform.py", "-p", f"{get_app_path()}", "-n", "Deep-STORM_2D_ZeroCostDL4Mic.ipynb", "-s"]
+    python_command += section_to_remove
+
+    subprocess.run(python_command, cwd=repository_path)
+    subprocess.run(["mv", get_app_path().joinpath("colabless_Deep-STORM_2D_ZeroCostDL4Mic.ipynb"), get_app_path().joinpath("Deep-STORM_2D_ZeroCostDL4Mic.ipynb")])
+
+    # Remove the cloned DL4MicEverywhere repository
+    if os.name == 'nt':
+        os.system(f'rmdir /s /q "{to}"')
+    else:
+        # rmtree has no permission to do this on Windows
+        shutil.rmtree(repository_path) 
+
+def run():
+    from album.runner.api import get_args, get_app_path
+    import subprocess
+    import os
+
+    # Fetch arguments and paths
+    args = get_args()
+    app_path = get_app_path()
+    
+    # Path to the downloaded notebook
+    notebook_path = app_path.joinpath("Deep-STORM_2D_ZeroCostDL4Mic.ipynb")
+
+    # Ensure the notebook exists
+    assert notebook_path.exists(), "Notebook does not exist"
+
+    # Output path for running the notebook
+    output_path = args.path
+    os.makedirs(output_path, exist_ok=True)
+    print(f"Saving output to {output_path}")
+
+    # Set the LD_LIBRARY_PATH to allow TensorFlow to find the CUDA libraries
+    global gpu_access
+    if gpu_access:
+        os.environ["LD_LIBRARY_PATH"] = f"{os.environ['LD_LIBRARY_PATH']}:{os.environ['CONDA_PREFIX']}/lib"
+
+    # Optionally, launch the Jupyter notebook to show the results
+    subprocess.run(["jupyter", "lab", str(notebook_path)], cwd=str(output_path))
+
+if gpu_access:
+    channels = """
+- conda-forge
+- nvidia
+- anaconda
+- defaults
+"""
+    dependencies = """
+- python=3.10
+- cudatoolkit=11.8.0
+- cudnn=8.9.2
+- pip
+- pkg-config
+"""
+else:
+    channels = """
+- conda-forge
+- defaults
+"""
+    dependencies = f"""
+- python=3.10
+- pip
+- pkg-config
+"""
+
+env_file = f"""
+channels:
+{channels}
+dependencies:
+{dependencies}
+- pip:
+    - GitPython==3.1.43 
+    - Pillow==9.4.0
+    - astropy==5.2.2
+    - fpdf2==2.7.4
+    - google==2.0.3
+    - h5py==3.8.0
+    - ipywidgets==8.0.7
+    - matplotlib==3.7.1
+    - numba==0.56.4
+    - numexpr==2.8.4
+    - numpy==1.22.4
+    - pandas==1.5.3
+    - pip==23.1.2
+    - scikit-image==0.19.3
+    - scikit-learn==1.2.2
+    - scipy==1.10.1
+    - tensorflow==2.12.0
+    - tqdm==4.65.0
+    - nbformat==5.9.2
+    - jupyterlab==3.4.0
+"""
+
+setup(
+    group="DL4MicEverywhere",
+    name="deep-storm-2d-zerocostdl4mic",
+    version="1.13.3",
+    solution_creators=["DL4MicEverywhere team", "album team"],
+    title="deep-storm-2d-zerocostdl4mic implementation.",
+    description="Single Molecule Localization Microscopy (SMLM) image reconstruction from high-density emitter data. Deep-STORM is a neural network capable of image reconstruction from high-density single-molecule localization microscopy (SMLM), first published in 2018 by Nehme et al. in Optica. This network allows image reconstruction of 2D super-resolution images, in a supervised training manner. The network is trained using simulated high-density SMLM data for which the ground-truth is available. These simulations are obtained from random distribution of single molecules in a field-of-view and therefore do not imprint structural priors during training. The network output a super-resolution image with increased pixel density (typically upsampling factor of 8 in each dimension). Note - visit the ZeroCostDL4Mic wiki to check the original publications this network is based on and make sure you cite these.",
+    documentation="https://raw.githubusercontent.com/HenriquesLab/ZeroCostDL4Mic/master/BioimageModelZoo/README.md",
+    tags=['AMD64', 'colab', 'notebook', 'Deep-STORM', 'labelling', 'ZeroCostDL4Mic', '2D', 'dl4miceverywhere'],
+    args=[{
+        "name": "path",
+        "type": "string",
+        "default": ".",
+        "description": "What is your working path?"
+    }],
+    cite=[{'doi': 'https://doi.org/10.1038/s41467-021-22518-0', 'text': 'von Chamier, L., Laine, R.F., Jukkala, J. et al. Democratising deep learning for microscopy with ZeroCostDL4Mic. Nat Commun 12, 2276 (2021). https://doi.org/10.1038/s41467-021-22518-0'}, {'doi': 'https://doi.org/10.1364/OPTICA.5.000458', 'text': 'Elias Nehme, Lucien E. Weiss, Tomer Michaeli, and Yoav Shechtman. Deep-STORM: super-resolution single-molecule microscopy by deep learning. Optica 5, 458-464 (2018)'}],
+    album_api_version="0.5.1",
+    covers=[],
+    run=run,
+    install=install,
+    dependencies={"environment_file": env_file},
+)
